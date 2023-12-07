b246f00e12d038720fb254cffb8a399f5124dc94
KOPOTbIWKA <150610864+KOPOTbIWKA@users.noreply.github.com>
2023-12-07 18:22:29 +0300

    ТЕПЕРЬ ТОЧНО РАБОТАЕТ ГЕНЕРАЦИЯ IP

--- ARNetworkAssistant/Assets/Scripts/Laptop.cs
+++ ARNetworkAssistant/Assets/Scripts/Laptop.cs
@@ -1,32 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class Laptop : MonoBehaviour
 {
-    private TMP_Text ipAddressText;
-
-    private void Awake()
-    {
-        ipAddressText = GetComponentInChildren<TMP_Text>(true);
-    }
-
-    public void SetIPAddress(string ipAddress)
-    {
-        if (ipAddressText != null)
-        {
-            ipAddressText.text = ipAddress;
-        }
-    }
+    private TMP_Text ipAddressText; // Reference to the TextMeshPro component for displaying IP address
 
     private void Start()
     {
-        
+        // Get the TextMeshPro component from the child named "IPAddress"
+        ipAddressText = transform.Find("IPAddress").GetComponent<TMP_Text>();
+
+        // Generate and set the IP address when the Laptop object is created
+        GenerateAndSetIPAddress();
+    }
+
+    private void GenerateAndSetIPAddress()
+    {
+        // Generate a random IP address with the format "XXX.XXX.XXX.XXX"
+        string ipAddress = GenerateRandomIPAddress();
+
+        // Set the generated IP address to the TextMeshPro component
+        ipAddressText.text = ipAddress;
+    }
+
+    private string GenerateRandomIPAddress()
+    {
+        // Generate four random numbers in the range 1-255 to represent each octet of the IP address
+        int octet1 = Random.Range(1, 256);
+        int octet2 = Random.Range(1, 256);
+        int octet3 = Random.Range(1, 256);
+        int octet4 = Random.Range(1, 256);
+
+        // Combine the octets into the IP address format
+        string ipAddress = $"{octet1}.{octet2}.{octet3}.{octet4}";
+
+        return ipAddress;
     }
 
     private void Update()
     {
-        
+        // You can add any additional update logic here if needed
     }
 }
--- ARNetworkAssistant/Assets/Scripts/LaptopPoolExample.cs
+++ ARNetworkAssistant/Assets/Scripts/LaptopPoolExample.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-
 public class LaptopPoolExample : MonoBehaviour
 {
     [SerializeField]
@@ -10,27 +9,14 @@ public class LaptopPoolExample : MonoBehaviour
     [SerializeField]
     private bool autoExpand = false;
     [SerializeField]
-    private Laptop laptopPrefab;
+    private Laptop laptop;
     [SerializeField]
     private GameObject planeMarkerPrefab;
-
     private PoolMono<Laptop> pool;
-
     private void Start()
     {
-        pool = new PoolMono<Laptop> (laptopPrefab, poolCnt,transform);
+        pool = new PoolMono<Laptop> (laptop,poolCnt,transform);
         pool.autoExpand = autoExpand;
-
-        GenerateAndSetIPAddresses();
-    }
-    private void GenerateAndSetIPAddresses()
-    {
-        foreach (var laptop in pool.GetPool())
-        {
-            string ipAddress = GenerateIPAddress();
-
-            laptop.SetIPAddress(ipAddress);
-        }
     }
     public void CreateLaptop()
     {
@@ -38,15 +24,8 @@ public class LaptopPoolExample : MonoBehaviour
         {
             var pos = planeMarkerPrefab.transform.position;
             var freeLaptop = pool.GetFreeElement();
-
             freeLaptop.transform.position = pos;
             planeMarkerPrefab.SetActive(true);
         }
-    }
-
-    private string GenerateIPAddress()
-    {
-        string ipAddress = $"{Random.Range(1, 256)}.{Random.Range(1, 256)}.{Random.Range(1, 256)}.{Random.Range(1, 256)}";
-        return ipAddress;
     }
 }
